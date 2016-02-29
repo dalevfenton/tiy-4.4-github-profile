ec4670c13415b54ff2d8846f0ce871ad8674d782
Dale Fenton <fentondale@gmail.com>
2016-02-28 21:29:38 -0500

    set static port on live-server styling up and refining window flow

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -27,15 +27,6 @@ var clientSecret = cache.clientSecret;
 var oAuthURL = 'https://github.com/login/oauth/authorize';
 var loggedIn = false;
 
-if(loggedIn){
-  loadPage();
-}else{
-  displayLogin();
-}
-
-function displayLogin(){
-  $('#login-modal').modal('show');
-}
 //HANDLEBARS TEMPLATE HELPER FUNCTIONS
 Handlebars.registerHelper('scale-bar', function( object, scale ) {
   return new Handlebars.SafeString( object * scale );
@@ -46,51 +37,41 @@ Handlebars.registerHelper('percent-offset', function(object) {
 
 if(document.URL.indexOf('?code=') > -1){
   var code = document.URL.match(/\?code=(.*)/);
-  console.log(code);
   $.getJSON('http://localhost:9999/authenticate/'+code[1], function(data) {
     // SET OUR AUTH TOKEN IN THE HEADER OF OUR API REQUESTS
-    if(typeof(githubtoken) !== "undefined"){
+    if(typeof(data.token) !== "undefined"){
       $.ajaxSetup({
         headers: {
           'Authorization': 'token ' + data.token
         }
       });
+      // console.log(window);
+      $('.loading-login-inner').html('<h2>Loading Your Page This Can Take a Few Seconds</h2><i class="fa fa-refresh fa-spin fa-3x fa-fw margin-bottom"></i>');
+      buildPage();
     }
   });
+}else{
+  oAuthLogin();
 }
 
 function oAuthLogin(){
   $('#github-login').click(function(event){
-    console.log('inside login handler');
-    window.open('https://github.com/login/oauth/authorize?client_id='+clientID+'&scope=repo');
+    window.location.replace('https://github.com/login/oauth/authorize?client_id='+clientID+'&scope=repo');
   });
 }
 
-
-//flag that can be set to api to use live api calls or cache to use cached data
-//so that our usage and refresh time are low
-var source = 'cache';
 //------------------------------------------------------------------------------
 //                        TEMPLATE IMPORTS
 //------------------------------------------------------------------------------
-//grab the header template for use
 var header = require('./header.handlebars');
 var sidebar = require('./sidebar.handlebars');
 var repos = require('./repos.handlebars');
 var newrepo = require('./newrepo.handlebars');
 var reposuccess = require('./reposuccess.handlebars');
-//build context obj
-//------------------------------------------------------------------------------
-//                        BUILD CONTEXT OBJECTS
-//------------------------------------------------------------------------------
-
-
-
 
 //------------------------------------------------------------------------------
-//                        INTERACTIVE EVENT HANDLERS
+//         EVENT HANDLERS WITH ELEMENTS NOT LOADED BY TEMPLATES
 //------------------------------------------------------------------------------
-
 //EVENT HANDLER FOR THE SORT OPTIONS OF THE REPO TAB
 $('.repo-sort-option').click(function(event){
     event.preventDefault();
@@ -131,6 +112,25 @@ $('#new-repo').click(function(event){
     drawNewRepoModal(userReturn);
   }
 });
+
+
+//------------------------------------------------------------------------------
+//                     EVENT HANDLER CALLBACK FUNCTIONS
+//      THESE ARE CALLED ONCE OUR TEMPLATES LOAD THE RESPECTIVE ELEMENTS
+//------------------------------------------------------------------------------
+function clearHover(clicked){
+  $(clicked).find('.hra-hover-box').css({'display': 'none'});
+}
+function dropDownClick( clicked ){
+  if($(clicked).find('ul').css('display') == 'none'){
+    $(clicked).find('ul').css({'display': 'inline-block'});
+  }else{
+    $(clicked).find('ul').css({'display': 'none'});
+  }
+  clearHover(clicked);
+}
+//EVENT HANDLER TO POPULATE AND LISTEN FOR DROPDOWNS USED
+//ON THE NEW REPOSITORY FORM
 function setDropDownEvents(array){
   array.forEach(function(item){
     var strOne = '#repo-' + item + '-list li a';
@@ -144,77 +144,43 @@ function setDropDownEvents(array){
     });
   });
 }
-
 //------------------------------------------------------------------------------
-//                  EVENT HANDLER CALLBACK FUNCTIONS
+//               BUILD OUR DATA OBJECTS FROM THE API ENDPOINTS
+//                  AND THEN LOAD OUR PAGE FROM TEMPLATES
 //------------------------------------------------------------------------------
-function clearHover(clicked){
-  $(clicked).find('.hra-hover-box').css({'display': 'none'});
-}
-function dropDownClick( clicked ){
-  if($(clicked).find('ul').css('display') == 'none'){
-    $(clicked).find('ul').css({'display': 'inline-block'});
-  }else{
-    $(clicked).find('ul').css({'display': 'none'});
-  }
-  clearHover(clicked);
-}
-//------------------------------------------------------------------------------
-//                  RUN THE APP
-//------------------------------------------------------------------------------
-var url = 'https://api.github.com/users/dalevfenton';
-if(source==='api'){
-  $.ajax(url).
-    done(function(data){
-      userReturn = data;
-      userReturn = prettyDate(userReturn);
-      drawHeader(userReturn);
-      var orgsUrl = 'https://api.github.com/user/orgs';
-      $.ajax(orgsUrl).done(function(data){
-        user_info.organizations = data;
-        // console.log(user_info);
-        drawSidebar(userReturn);
-        drawNewRepoModal(userReturn);
-        var repoUrl = 'https://api.github.com/users/' + userReturn.login + '/repos';
-        $.ajax(repoUrl).done(function(data){
-          var index = 0;
-          // console.log(data);
-          reposReturned = data;
-          reposReturned.forEach(function( item ){
-            var dateFrom = timeSince( item.updated_at );
-            item.time = dateFrom[0];
-            item.period = dateFrom[1];
-            item.secondsSince = dateFrom[2];
-          });
-          reposReturned =  _.sortBy(reposReturned, 'secondsSince');
-          recurseRepos(reposReturned, index);
-        });
-      });
-  })
-  .fail(function(jqXHR, status, error){
-    console.log(jqXHR);
-    console.log(status);
-    console.log(error);
+//entry point for our page once we have an auth token
+function buildPage(){
+  var authURL = 'https://api.github.com/user';
+  $.ajax(authURL).done(function(data){
+    userReturn = prettyDate(data);
+    getOrgs();  //ajax call to organizations api endpoint
   });
-}else{
-  userReturn = prettyDate(userReturn);
-  reposReturned.forEach(function( item ){
-    var dateFrom = timeSince( item.updated_at );
-    item.time = dateFrom[0];
-    item.period = dateFrom[1];
-    item.secondsSince = dateFrom[2];
-  });
-  reposReturned = _.sortBy(reposReturned, 'secondsSince');
-  repoLoaded = true;
-  drawHeader(userReturn);
-  drawSidebar(userReturn);
-  drawRepos(reposReturned);
-  userReturn.licenses = licenses;
-  userReturn.gitignores = gitignores;
-  drawNewRepoModal(userReturn);
-  //EVENT HANDLER FOR GITHUB LOGIN WITH OAUTH
-  oAuthLogin();
+}
 
+function getOrgs(){
+  var orgsUrl = 'https://api.github.com/user/orgs';
+  $.ajax(orgsUrl).done(function(data){
+    userReturn.organizations = data;
+    getRepos();  //ajax call to repos api endpoint
+  });
+}
+
+function getRepos(){
+  var repoUrl = 'https://api.github.com/users/' + userReturn.login + '/repos';
+  $.ajax(repoUrl).done(function(data){
+    reposReturned = data;
+    reposReturned.forEach(function( item ){
+      var dateFrom = timeSince( item.updated_at );
+      item.time = dateFrom[0];
+      item.period = dateFrom[1];
+      item.secondsSince = dateFrom[2];
+    });
+    reposReturned =  _.sortBy(reposReturned, 'secondsSince');
+    recurseRepos(reposReturned, 0);
+    //recurseRepos recursively does an ajax call to the individual
+    //repo api endpoint for each repo we retrieved to get more indepth
+    //data for those repos
+  });
 }
 
 function recurseRepos(repoArr, counter){
@@ -226,6 +192,9 @@ function recurseRepos(repoArr, counter){
       recurseRepos(repoArr, counter);
     });
   }else{
+    //if we've gotten through the entire array of repos, then do another
+    //call through our array to get statistics for them as well with
+    //recurseRepoStats
     counter = 0;
     reposReturned = repoArr;
     recurseRepoStats(repoArr, counter);
@@ -241,12 +210,75 @@ function recurseRepoStats(repoArr, counter){
           recurseRepoStats(repoArr, counter);
         });
     }else{
-      console.log(repoArr);
+      //set our global repo variable we pass into the Handlebars template
       reposReturned = repoArr;
+      //set our global flag to tell other functions we have loaded our data
       repoLoaded = true;
+      //send our data out to populate the templates
+      drawHeader(userReturn);
+      drawSidebar(userReturn);
+      drawNewRepoModal(userReturn);
       drawRepos(reposReturned);
+      $('#loading-login').css("bottom", "100%");
     }
 }
+
+// var url = 'https://api.github.com/users/dalevfenton';
+// if(source==='api'){
+//   $.ajax(url).
+//     done(function(data){
+//       userReturn = data;
+//       userReturn = prettyDate(userReturn);
+//       drawHeader(userReturn);
+//       var orgsUrl = 'https://api.github.com/user/orgs';
+//       $.ajax(orgsUrl).done(function(data){
+//         user_info.organizations = data;
+//         // console.log(user_info);
+//         drawSidebar(userReturn);
+//         drawNewRepoModal(userReturn);
+//         var repoUrl = 'https://api.github.com/users/' + userReturn.login + '/repos';
+//         $.ajax(repoUrl).done(function(data){
+//           var index = 0;
+//           // console.log(data);
+//           reposReturned = data;
+//           reposReturned.forEach(function( item ){
+//             var dateFrom = timeSince( item.updated_at );
+//             item.time = dateFrom[0];
+//             item.period = dateFrom[1];
+//             item.secondsSince = dateFrom[2];
+//           });
+//           reposReturned =  _.sortBy(reposReturned, 'secondsSince');
+//           recurseRepos(reposReturned, index);
+//         });
+//       });
+//   })
+//   .fail(function(jqXHR, status, error){
+//     console.log(jqXHR);
+//     console.log(status);
+//     console.log(error);
+//   });
+// }else{
+//   userReturn = prettyDate(userReturn);
+//   reposReturned.forEach(function( item ){
+//     var dateFrom = timeSince( item.updated_at );
+//     item.time = dateFrom[0];
+//     item.period = dateFrom[1];
+//     item.secondsSince = dateFrom[2];
+//   });
+//   reposReturned = _.sortBy(reposReturned, 'secondsSince');
+//   repoLoaded = true;
+//   drawHeader(userReturn);
+//   drawSidebar(userReturn);
+//   drawRepos(reposReturned);
+//   userReturn.licenses = licenses;
+//   userReturn.gitignores = gitignores;
+//   drawNewRepoModal(userReturn);
+//   //EVENT HANDLER FOR GITHUB LOGIN WITH OAUTH
+//   oAuthLogin();
+//
+// }
+
+
 //------------------------------------------------------------------------------
 //                 TEMPLATE FUNCTIONS CALLED ON AJAX COMPLETION
 //------------------------------------------------------------------------------
@@ -256,13 +288,24 @@ function drawHeader(data){
   $('#hra-profile, #hra-create-new').click(function(){
     dropDownClick(this);
   });
-  $('#hra-profile, #hra-create-new').on('mouseover', function(){
-    // window.preventDefault();
-      $(this).find('.hra-hover-box').css({'display': 'block'});
+  // $('#hra-profile, #hra-create-new').on('mouseover', function(){
+  //     $(this).find('.hra-hover-box').css({'display': 'block'});
+  // });
+  // $('#hra-profile, #hra-create-new').on('mouseout', function(){
+  //     clearHover(this);
+  // });
+  $('#new-repo-dd').click(function(event){
+    event.preventDefault();
+    $('.bs-example-modal-lg').modal('toggle');
   });
-  $('#hra-profile, #hra-create-new').on('mouseout', function(){
-    // window.preventDefault();
-      clearHover(this);
+  $('#logout').click(function(event){
+    event.preventDefault();
+    $.ajaxSetup({
+      headers: {}
+    });
+    $('.loading-login-inner').html('<button type="button" id="github-login" class="btn btn-success"><span class="glyphicon glyphicon-book" aria-hidden="true"></span> Login To Your GitHub Account</button>');
+    $('#loading-login').css("bottom", "0");
+    oAuthLogin();
   });
 }
 
@@ -276,12 +319,6 @@ function drawSidebar(data){
 function drawRepos(data){
   var repoHTML = repos({'repos': data});
   $('#repo-listings').html(repoHTML);
-}
-
-function prettyDate(data){
-  var date = new Date(data.created_at);
-  data.pretty_date = monthNames[date.getMonth()] + " " + date.getDate() + ", " + date.getFullYear();
-  return data;
 }
 
 function drawNewRepoModal(data){
@@ -320,7 +357,6 @@ function drawNewRepoModal(data){
       "dataType": "json",
       "data": JSON.stringify({ "name": repoName, "description": repoDesc, "private":repoPubPriv, "auto_init": repoInit, "gitignore_template": repoGitIg, "license_template": repoLicense})
     }).done(function(data){
-      console.log(data);
       newrepoReturned = data;
       drawRepoResult(newrepoReturned);
       var dateFrom = timeSince( newrepoReturned.updated_at );
@@ -332,9 +368,9 @@ function drawNewRepoModal(data){
       drawRepos(reposReturned);
     }).fail(function(jqXHR, status, error){
       newrepoReturned = jqXHR;
-      console.log(jqXHR);
-      console.log(status);
-      console.log(error);
+      // console.log(jqXHR);
+      // console.log(status);
+      // console.log(error);
       drawRepoResult(newrepoReturned);
     });
   });
@@ -344,6 +380,16 @@ function drawRepoResult(data){
   var repoSuccessHTML = reposuccess(data);
   $('.modal-content').html(repoSuccessHTML);
 }
+
+function prettyDate(data){
+  var date = new Date(data.created_at);
+  data.pretty_date = monthNames[date.getMonth()] + " " + date.getDate() + ", " + date.getFullYear();
+  return data;
+}
+
+//------------------------------------------------------------------------------
+//              UTILITY FUNCTIONS USED TO FORMAT OR SORT DATA
+//------------------------------------------------------------------------------
 //from stackoverflow by rob updating from Sky Sanders
 //http://stackoverflow.com/questions/3177836/how-to-format-time-since-xxx-e-g-4-minutes-ago-similar-to-stack-exchange-site/23259289#23259289
 function timeSince(date) {
